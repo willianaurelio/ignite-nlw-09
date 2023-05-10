59ea78a2dd538c628d650a8faf55b37889555e47
willianaurelio <willianaurelio@live.com>
2023-05-10 14:23:17 -0300

    nlw #2/5 - part-1

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,9 +5,14 @@ export default {
     './index.html'
   ],
   theme: {
+    fontFamily:{
+      sans: ['Inter','sans-serif']  
+    },
     extend: {
       backgroundImage: {
-        galaxy: "url('/background-galaxy.png')"
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(90deg, #9572FC 5.08%, #43E7AD 59.94%, #E1D55D 91.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.9) 67%)',
       },
     },
   },
